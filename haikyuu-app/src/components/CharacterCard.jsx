fd51c79adaff9e6e860b52081ee9caefa183b538
emiHuy <emilyhuynh06@gmail.com>
2025-06-30 20:52:26 -0400

    feat: create character card and display them in alphabetical order

---
+++ haikyuu-app/src/components/CharacterCard.jsx
@@ -0,0 +1,15 @@
+import '../css/CharacterCard.css'
+
+function CharacterCard({character}) {
+    let [firstName, lastName] = character.name.split(' ');
+    return (
+        <div className='character-card'>
+            <img src={character.image} width='250' height='250'/>
+            <h3>{firstName}</h3>
+            <h2>{lastName}</h2>
+            <p>{character.team}</p>
+        </div>
+    )
+}
+
+export default CharacterCard
---
+++ haikyuu-app/src/pages/Characters.jsx
@@ -0,0 +1,18 @@
+import { characters } from '../data/data';
+import CharacterCard from '../components/CharacterCard';
+import '../css/Characters.css'
+
+function Characters() {
+    const chars = characters
+    chars.sort((a, b) => a.name.split(' ')[1].localeCompare(b.name.split(' ')[1]))
+    
+    return (
+        <div className='characters'>
+            {chars.map((char) => (
+                <CharacterCard key={char.id} character={char} />
+            ))}
+        </div>
+    )
+}
+
+export default Characters
